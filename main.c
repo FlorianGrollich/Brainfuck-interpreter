815ab546539354b3e4edf4748267c90e7e5724ed
FlorianGrollich <F-Grollich@web.de>
2023-04-20 15:45:21 +0200

    add tokentypes

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define TAPE_SIZE 30000
+
+enum TokenType {
+    INCREMENT,
+    DECREMENT,
+    LEFT,
+    RIGHT,
+    INPUT,
+    OUTPUT,
+    LOOP_START,
+    LOOP_END
+};
 
 int main() {
+    unsigned char tape[TAPE_SIZE] = {0};
+    //pointer to the current cell
+    char prt = 0;
 
     return 0;
 }
