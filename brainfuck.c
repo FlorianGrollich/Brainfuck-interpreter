1b3163e5e2cc847776762affab27c4312646c0cc
FlorianGrollich <F-Grollich@web.de>
2023-04-22 14:11:48 +0200

    add parsing function

--- brainfuck.c
+++ brainfuck.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "brainfuck.h"
 
+
 enum TokenType getType(char input) {
     switch (input) {
         case '<':
@@ -22,28 +23,46 @@ enum TokenType getType(char input) {
         case ']':
             return LOOP_END;
         default:
-            break;
+            return COMMENT;
     }
 }
 
 
-struct Node parsing(char* input) {
-    for(int i = 0; i < strlen(input); i++) {
-
-
-
-    }
-
+struct Node* createNode(char input) {
+    struct Node* node = malloc(sizeof(struct Node));
+    node->type = getType(input);
+    node->next = NULL;
+    node->child = NULL;
+    return node;
 }
 
 
+struct Node* parsing(char* input) {
+    struct Node* head = NULL;
+    struct Node* prevNode = NULL;
+    for (int i = 0; input[i] != '\0'; i++) {
+        struct Node* currentNode = createNode(input[i]);
+        if (prevNode != NULL) {
+            prevNode->next = currentNode;
+        }
+        prevNode = currentNode;
+        if (head == NULL) {
+            head = currentNode;
+        }
+    }
+    return head;
+}
 
 int main() {
     unsigned char tape[TAPE_SIZE] = {0};
     //pointer to the current cell
     char prt = 97;
-
-
-
+    char* input = "+>[-]<.";
+    struct Node* head = parsing(input);
+    struct Node* currentNode = head;
+    while (currentNode != NULL) {
+        printf("%d ", currentNode->type);
+        currentNode = currentNode->next;
+    }
     return 0;
 }
